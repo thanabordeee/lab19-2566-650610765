143e6c9877157b50e27bc9e69c3136b52d87d76b
thanabordeee <jejekup00123@hotmail.co.th>
2023-09-18 15:24:49 +0700

    complete

--- src/app/page.js
+++ src/app/page.js
@@ -36,10 +36,13 @@ export default function Home() {
   };
 
   const loadMyCourses = async () => {
+    setLoadingMyCourses(true);
     const resp = await axios.get("/api/enrollment", {
       headers: { Authorization: `Bearer ${token}` },
+    
     });
     setMyCourses(resp.data.courses);
+    setLoadingMyCourses(false);
   };
 
   useEffect(() => {
@@ -54,11 +57,17 @@ export default function Home() {
 
   const login = async () => {
     try {
+      setLoadingLogin(true);
+      
       const resp = await axios.post("/api/user/login", { username, password });
       setToken(resp.data.token);
       setAuthenUsername(resp.data.username);
       setUsername("");
       setPassword("");
+      localStorage.setItem("token", resp.data.token);
+      localStorage.setItem("username", resp.data.username);
+      setLoadingLogin(false);
+      
     } catch (error) {
       if (error.response.data) {
         alert(error.response.data.message);
@@ -70,6 +79,8 @@ export default function Home() {
     setAuthenUsername(null);
     setToken(null);
     setMyCourses(null);
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
   };
 
   return (
@@ -105,7 +116,9 @@ export default function Home() {
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
               />
-              <Button onClick={login}>Login</Button>
+              {!loadingLogin && (<Button onClick={login}>Login</Button>)}
+              {loadingLogin && (<Button disabled={loadingLogin}>Login...</Button>)}
+               {/* <Button disabled={loadingLogin}>Login</Button> */}
             </Group>
           )}
           {authenUsername && (
@@ -133,9 +146,9 @@ export default function Home() {
             ))}
 
           {/* Do something with below loader!! */}
-          <Loader variant="dots" />
+         {loadingMyCourses && (<Loader variant="dots" />)} 
         </Paper>
-        <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year="2023" fullName="Thanabordee Sornchai" studentId="650610765" />
       </Stack>
     </Container>
   );
